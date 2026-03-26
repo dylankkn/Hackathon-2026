6850af4b0b51f4320e98e2c8038460c8f6b96f92
dylankkn <dylankkn@gmail.com>
2026-03-26 04:19:53 -0300

    first commit

---
+++ config.py
@@ -0,0 +1,37 @@
+from dotenv import load_dotenv
+import os
+from enum import Enum
+
+# --- CONFIGURAÇÃO DA IA ---
+load_dotenv()
+API_KEY = os.environ.get("API_KEY")
+
+class Modelos(Enum):
+    DeepSeek = "tngtech/deepseek-r1t2-chimera:free"
+    Nemotron_3_Super = "nvidia/nemotron-3-super-120b-a12b:free"
+    StepFun_35_FLASH = "stepfun/step-3.5-flash:free"
+
+    @property
+    def valor(self):
+        modelos = {
+            "DeepSeek": {
+                "name": "deepseek-r1t2-chimera",
+                "model": "tngtech/deepseek-r1t2-chimera:free",
+                "provider": "deepseek"
+            },
+            "Nemotron_3_Super": {
+                "name": "Nemotron_3_Super",
+                "model": "nvidia/nemotron-3-super-120b-a12b:free",
+                "provider": "NVIDIA"
+            },
+            "StepFun_35_FLASH": {
+                "name": "Step-3.5-flash",
+                "model": "stepfun/step-3.5-flash:free",
+                "provider": "StepFun"
+            }
+        }
+        return modelos[self.name]
+
+# --- CONFIGURAÇÃO DA SERIAL ---
+porta_arduino = 'COM24' 
+baud_rate = 9600
---
+++ main.py
@@ -0,0 +1,30 @@
+import serial
+import time
+from quiz_ia import iniciar_quiz
+
+
+
+# --- CONFIGURAÇÃO DA SERIAL ---
+porta_arduino = 'COM24' 
+baud_rate = 9600
+
+
+# --- LOOP PRINCIPAL ---
+try:
+    arduino = serial.Serial(porta_arduino, baud_rate, timeout=1)
+    print(f"✅ Conectado ao Arduino na {porta_arduino}!")
+
+    while True:
+        if arduino.in_waiting > 0:
+            msg = arduino.readline().decode('utf-8', errors='ignore').strip()
+            if msg == "ACESSO_VERDE":
+                iniciar_quiz()
+
+except serial.SerialException:
+    print("❌ Erro: Porta COM ocupada! Verifique o Monitor Serial do Arduino.")
+except KeyboardInterrupt:
+    print("\nEncerrando...")
+finally:
+    if 'arduino' in locals():
+        arduino.close()
+
